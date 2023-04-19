dc24582c53385e685cd0e1d89faa36b7b094e87e
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-19 16:31:51 -0400

    use state implementation on About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useState } from "react";
 
 export default function About () {
-  const [imgUrl, setImgUrl] = useState('https://picsum.photos/300/300');
+  const [imgUrl, setImgUrl] = useState('https://picsum.photos/300/300?random=2');
 
   return(
     <main>
@@ -15,6 +15,11 @@ export default function About () {
           </Col>
 
           <Col>
+            <br/>
+             <Button onClick={() => setImgUrl('https://picsum.photos/100/100?random=1')}>100x100</Button>
+             <Button onClick={() => setImgUrl('https://picsum.photos/300/300?random=2')}>300x300</Button>
+             <Button onClick={() => setImgUrl('https://picsum.photos/500/500?random=3')}>500x500</Button>
+             <br/>
             <h1 className="mt-3">Your Name</h1>
             <p>If you enjoy using big fonts, you're not alone! There are many reasons why someone might prefer larger text when reading or writing. For example, larger fonts can make text easier to read, especially for people with visual impairments or those who struggle with reading small text. Additionally, big fonts can help draw attention to important information, making it easier to spot and understand.</p>
           </Col>
